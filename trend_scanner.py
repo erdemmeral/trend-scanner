ca110244d281197bd01dddd70aa974e47e2cffb1
erdemmeral <erdem_meral@hotmail.com>
2025-01-04 11:54:30 +0300

    deneme

--- trend_scanner.py
+++ trend_scanner.py
@@ -1,1 +1,12 @@
- 
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler('debug.log')
+    ]
+)
+logger = logging.getLogger(__name__) 
